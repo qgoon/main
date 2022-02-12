61f50c7e240fc30d6b001e96fe8cb5f5b277de91
Manarbek <qzgoon.@gmail.com>
2022-02-12 16:01:34 +0600

    add task_2.c

---
+++ task_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double main (int argc, char* argv[])
+{
+	short i = 0;
+	short count_vowels = 0;
+	char array_vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+	char str[100];
+	scanf("%s", str);
+	while(str[i] != '\0')
+	{
+		short j = 0;
+		while(array_vowels[j] != 4)
+		{
+			if(array_vowels[j] == str[i])
+			{
+				count_vowels++;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("%d", count_vowels);
+}
