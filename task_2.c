2e15e056504f94e3479f78da1ef256229550cdac
Manarbek <qzgoon.@gmail.com>
2022-02-12 16:21:13 +0600

    add func gets for input string

--- task_2.c
+++ task_2.c
@@ -6,12 +6,13 @@ double main (int argc, char* argv[])
 	short count_vowels = 0;
 	char array_vowels[5] = {'a', 'e', 'i', 'o', 'u'};
 	char str[100];
-	scanf("%s", str);
+	gets(str);
 	while(str[i] != '\0')
 	{
 		short j = 0;
 		while(array_vowels[j] != 4)
 		{
+			if(str[i] == ' ') break;
 			if(array_vowels[j] == str[i])
 			{
 				count_vowels++;
